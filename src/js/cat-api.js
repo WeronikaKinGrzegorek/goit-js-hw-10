6af13bacefb9767822d0138903e1d22630601d22
WeronikaKinGrzegorek <126391598+WeronikaKinGrzegorek@users.noreply.github.com>
2023-08-30 10:09:17 +0200

    first attempt

---
+++ src/js/cat-api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_uaBet4kZnlpPyoO8jLxecvDHBAoAm6OfibtypPsDdd0woAwCIw5VzQaEnGpHNegu';
+
+export function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => {
+      new Error('Oops! Something went wrong! Try reloading the page!');
+    });
+}
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import { fetchBreeds } from './cat-api';
+
+const breedSelect = document.querySelector('.breed-select');
+
+fetchBreeds().then(breeds => {
+  for (let i = 0; i < breeds.length; i++) {
+    const breed = breeds[i];
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = breed.name;
+    breedSelect.appendChild(option);
+  }
+});
